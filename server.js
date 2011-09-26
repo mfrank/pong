ced93782325f36512b809bf8947cf5c49f5de5c2
Martin Frank <m.frank@curasystems.de>
2011-09-26 16:38:11 +0200

    removed semis

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
 
-app.listen(8080);
+app.listen(8080)
 
 function handler (req, res) {
   fs.readFile(__dirname + '/pong.html',
@@ -13,7 +13,7 @@ function handler (req, res) {
       }
       res.writeHead(200)
       res.end(data);
-    });
+    })
 }
 
 var playerA = null
