79889da8d00f3ede59a2e9d47e699b3594d97c22
Martin Frank <m.frank@curasystems.de>
2011-09-26 16:11:43 +0200

    worked on it a little bit

--- server.js
+++ server.js
@@ -8,15 +8,12 @@ function handler (req, res) {
   fs.readFile(__dirname + '/pong.html',
     function (err, data) {
       if (err) {
-        res.writeHead(500);
-        return res.end('Error loading pong.html');
+        res.writeHead(500)
+        return res.end('Error loading pong.html')
       }
-      console.log("jo hier")
-      res.writeHead(200);
+      res.writeHead(200)
       res.end(data);
     });
-
-  
 }
 
 var playerA = null
@@ -30,21 +27,21 @@ io.sockets.on('connection', function (socket) {
      playerA = socket
      socket.emit('accepted', { player: 1 })
      console.log("player 1 connected")
- 
+   
      playerA.on('disconnect', function (){
        console.log("disconnect player 1")
        playerA = null
      })
 
      playerA.on('move', function (data) {
-      console.log("Player A wants to go " + data.direction);
+      console.log("Player 1 wants to go " + data.direction);
       if (playerB != null){
         playerB.emit('move', data)
       }
     });
 
     playerA.on('start', function(){
-        console.log("Player A started");
+        console.log("Player 1 started the game");
       if (playerB != null){
         playerB.emit('start')
       }
@@ -65,16 +62,15 @@ io.sockets.on('connection', function (socket) {
      })
 
     
-     playerB.on('move', function (data) {
-      console.log("Player B wants to go " + data.direction);
+      playerB.on('move', function (data) {
+      console.log("Player 2 wants to go " + data.direction);
       if (playerA != null){
-      playerA.emit('move', data)
+        playerA.emit('move', data)
     }
     });
 
     playerB.on('start', function(){
-        console.log("Player B started");
-      if (playerA != null){
+      if (playerB != null){
         playerA.emit('start')
       }
     })
