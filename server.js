e2e2cfc42a2067d54236b8eee0a5dc9b860ea69a
Martin Frank <m.frank@curasystems.de>
2011-09-27 17:00:52 +0200

    fixed paddle bug

--- server.js
+++ server.js
@@ -28,6 +28,8 @@ io.sockets.on('connection', function (socket) {
      socket.emit('accepted', { player: 1 })
      console.log("player 1 connected")
    
+     playerA.on('ping')
+
      playerA.on('disconnect', function (){
        console.log("disconnect player 1")
        playerA = null
